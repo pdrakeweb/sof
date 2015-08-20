942c752180fef980d320a1f637e943567b04d9dc
Peter Drake <peter.drake@acquia.com>
2015-08-20 11:25:36 -0400

    Pretty print results.

--- lib/sof/checks/ssh.rb
+++ lib/sof/checks/ssh.rb
@@ -10,9 +10,26 @@ class Ssh < Sof::Check
     @sudo = check['sudo']
   end
 
+  def command
+    @sudo.nil? ? @command : "sudo -u #{@sudo} #{@command}"
+  end
+
   def run(server)
     ssh = Sof::Ssh.new(server)
-    ssh.exec(@command)
+    ssh_result = ssh.exec(command)
+
+    case ssh_result[:exitstatus]
+    when 255
+      check_title = "#{@name} SSH could not connect"
+    when 127
+      check_title = "#{@name} check not found"
+    else
+      check_title = "#{@name}"
+    end
+
+    check_status = ssh_result[:exitstatus] == 0 ? :pass : :fail
+    { check_title => {'status' => check_status, 'exit status' => ssh_result[:exitstatus], 'stdout' => ssh_result[:stdout].strip } }
+
   end
 
 end
--- lib/sof/runner.rb
+++ lib/sof/runner.rb
@@ -1,11 +1,11 @@
 module Sof
 class Runner
 
-  attr_accessor :server_concurrency, :check_concurrency, :manifest
+  attr_accessor :server_concurrency, :check_concurrency, :manifest, :results
 
   def initialize(manifest)
-    @server_concurrency ||= 2
-    @check_concurrency ||= 2
+    @server_concurrency ||= 10
+    @check_concurrency ||= 5
     @manifest = manifest
   end
 
@@ -19,16 +19,28 @@ class Runner
 
   def run_checks
     @results = []
-    #Parallel.map_with_index(servers, :in_processes => server_concurrency, :progress => 'Running checks') do |server|
-    result = Parallel.map_with_index(servers, :in_processes => server_concurrency) do |server|
+    @results = Parallel.map_with_index(servers, :in_processes => server_concurrency, :progress => 'Running checks') do |server|
       checks = Sof::Check.load(server.categories)
-      result = Parallel.map_with_index(checks, :in_threads => check_concurrency) do |check|
-        check.run(server)
+      check_results = Parallel.map_with_index(checks, :in_threads => check_concurrency) do |check|
+        { :check => check, :return => check.run(server) }
       end
+      { :server => server, :result => check_results }
     end
-    
-    pp result
   end
 
+  def output_results(verbose = false)
+    munged_output = {}
+    @results.each do |single_result|
+      check_results = []
+      check_results << "#{single_result[:result].size} checks completed"
+      single_result[:result].each do |check_result|
+        if check_result[:return].first[1]['status'] == :fail || verbose
+          check_results << check_result[:return]
+        end
+      end
+      munged_output[single_result[:server].hostname] = check_results
+    end
+    puts munged_output.to_yaml
+  end
 end
 end
