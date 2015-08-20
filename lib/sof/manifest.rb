ec459526e84c3d05b011ffa47b607af1aa993648
Thomas Elliot <thomas.elliot@acquia.com>
2015-08-20 13:34:02 -0400

    added standard io and custom error for manifest

--- lib/sof/manifest.rb
+++ lib/sof/manifest.rb
@@ -3,17 +3,24 @@ require 'yaml'
 module Sof
   class Manifest
     def self.get(path)
-      fail StandardError, "#{path} is not found" unless File.file?(path)
+      fail ManifestError, "#{path} is not found" unless File.file?(path)
 
       manifest = YAML.load_file(path)
 
-      fail StandardError, 'Either no port found or bad format in manifest' unless manifest.has_key?('port')
-      fail StandardError, 'Either no servers found or bad format in manifest' unless manifest.has_key?('servers')
+      fail ManifestError, 'Either no port found or bad format in manifest' unless manifest.has_key?('port')
+      fail ManifestError, 'Either no servers found or bad format in manifest' unless manifest.has_key?('servers')
 
       manifest['servers'].each do |server|
-        fail StandardError, 'Either no name found or bad format in servers in manifest' unless server.has_key?('name')
+        fail ManifestError, 'Either no name found or bad format in servers in manifest' unless server.has_key?('name')
       end
       manifest
+    end
+  end
+
+  class ManifestError < StandardError
+    attr_reader :object
+    def initialize(message)
+      super(message)
     end
   end
 end
