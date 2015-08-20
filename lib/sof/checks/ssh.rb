de4203181a4cb3205fea32f81bc16ba4fc62e96d
Peter Drake <peter.drake@acquia.com>
2015-08-20 16:09:03 -0400

    Truncate SSH command output.

--- lib/sof/checks/ssh.rb
+++ lib/sof/checks/ssh.rb
@@ -17,13 +17,20 @@ class Ssh < Sof::Check
     ssh = Sof::Ssh.new(server, echo: @options[:debug])
     ssh_result = ssh.exec(command)
 
-    case ssh_result[:exitstatus]
-    when 255
-      check_title = "#{@name} SSH could not connect"
-    when 127
-      check_title = "#{@name} check not found"
-    else
-      check_title = "#{@name}"
+      case ssh_result[:exitstatus]
+      when 255
+        check_title = "#{@name} SSH could not connect"
+      when 127
+        check_title = "#{@name} check not found"
+      else
+        check_title = "#{@name}"
+      end
+
+      extra_fields = { 'exit status' => ssh_result[:exitstatus], 'stdout' => truncate(ssh_result[:stdout]) }
+      check_status = ssh_result[:exitstatus] ==  @expected_result ? :pass : :fail
+    rescue Errno::ECONNREFUSED
+      check_title = "#{@name} connection refused"
+      check_status = :fail
     end
 
     check_status = ssh_result[:exitstatus] == @expected_result ? :pass : :fail
@@ -31,5 +38,12 @@ class Ssh < Sof::Check
 
   end
 
+  def truncate(s, length = 255, ellipsis = '...')
+    if s.length > length
+      s.to_s[0..length].gsub(/[^\w]\w+\s*$/, ellipsis)
+    else
+      s
+    end
+  end
 end
 end
