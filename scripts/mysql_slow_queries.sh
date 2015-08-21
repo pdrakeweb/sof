d08bdb3937fa0df7c1ac4d9135b88ac080a00023
Peter Drake <peter.drake@acquia.com>
2015-08-20 21:53:26 -0400

    Fix slow query output.

--- scripts/mysql_slow_queries.sh
+++ scripts/mysql_slow_queries.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mysql -s -e 'SELECT USER, HOST, DB, COMMAND, TIME, STATE, INFO FROM INFORMATION_SCHEMA.PROCESSLIST WHERE COMMAND = "Query" AND Time > 30' | grep '[a-z1-9]'
-exit $?
+SLOW_QUERIES=`mysql -s -e 'SELECT USER, HOST, DB, COMMAND, TIME, STATE, INFO FROM INFORMATION_SCHEMA.PROCESSLIST WHERE COMMAND = "Query" AND Time > 30'`
+EXIT_STATUS=`echo "$SLOW_QUERIES" | wc -l`
+echo "$SLOW_QUERIES" | sed -e "s/[[:space:]]\+/ /g"
+exit $EXIT_STATUS
